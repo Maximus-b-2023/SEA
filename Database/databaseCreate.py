615c3e68a7b6d5a0ca432584999b5177738cfd95
Maximus-b-2023 <Maximus.b_2023_man@outlook.com>
2025-05-31 22:07:11 +0100

    Created all tables and implemented foreign keys

--- Database/databaseCreate.py
+++ Database/databaseCreate.py
@@ -1,13 +1,38 @@
 import sqlite3
 
 def createUserTable():
-    conn = sqlite3.connect('./Database/user.db')
+    conn = sqlite3.connect('./Database/tables.db')
 
     conn.execute('''CREATE TABLE user
-                 (ID INT PRIMARY KEY     NOT NULL,
+                 (USERID INT PRIMARY KEY     NOT NULL,
                  USERNAME       TEXT    NOT NULL,
                  PASSWORD       TEXT    NOT NULL,
                  EMAIL          TEXT    NOT NULL);''')
     print("Table Created successfully")
 
+def createCropsTable():
+    conn = sqlite3.connect('./Database/tables.db')
+
+    conn.execute('''CREATE TABLE crops
+                 (CROPID INT PRIMARY KEY     NOT NULL,
+                 CROPNAME       TEXT    NOT NULL,
+                 SEEDPRICE       INT    NOT NULL,
+                 LOWESTSELLINGPRICE          INT    NOT NULL);''')
+    print("Table Created successfully")
+
+def createSalesTable():
+    conn = sqlite3.connect('./Database/tables.db')
+
+    conn.execute('''CREATE TABLE sales
+                 (SALEID INT PRIMARY KEY     NOT NULL,
+                 USERID  TEXT   NOT NULL,
+                 CROPNAME       TEXT    NOT NULL,
+                 SEASON         TEXT    NOT NULL,
+                 QUANTITYSOLD          TEXT    NOT NULL,
+                 PROFITMADE     INT     NOT NULL,
+                 FOREIGN KEY(USERID) REFERENCES user(USERID));''')
+    print("Table Created successfully")
+
 createUserTable()
+createCropsTable()
+createSalesTable()
