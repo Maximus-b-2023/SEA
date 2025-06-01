bd1dd694e6e21e40a9cf4dc029318b7868f13773
Maximus-b-2023 <Maximus.b_2023_man@outlook.com>
2025-06-01 01:18:05 +0100

    Created functions for determing cost of seeds and removed a file not currently in use

---
+++ Backend/costCalculators.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+conn = sqlite3.connect('./Database/tables.db')
+cur = conn.cursor()
+
+def getCropPrice(userinput):
+    sql = 'select LOWESTSELLINGPRICE from crops WHERE CROPNAME ="'+ userinput + '";'
+    cur.execute(sql)
+    cropValue = cur.fetchone()
+    return int(cropValue[0])
+
+def getSeedPrice(userinput):
+    sql = 'select SEEDPRICE from crops WHERE CROPNAME ="'+ userinput + '";'
+    cur.execute(sql)
+    seedPrice = cur.fetchone()
+    return int(seedPrice[0])
+
+def calcMinValue(cropName, quantity):
+    minValue = quantity * getCropPrice(cropName)
+    return minValue
+
+def calcSeedCost(cropName, quantity):
+    seedsCost = quantity * getSeedPrice(cropName)
+    return seedsCost
+
+print(calcSeedCost("Strawberry", 20))
+print(calcMinValue("Strawberry", 20))
